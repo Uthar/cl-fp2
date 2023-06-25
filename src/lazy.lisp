7038a2a907894f3aa8572b637c392a711574c4bd
Kasper Gałkowski <k@galkowski.xyz>
2023-06-25 13:05:38 +0200

    fp->api to be consistent with other code

--- src/lazy.lisp
+++ src/lazy.lisp
@@ -2,27 +2,27 @@
   (:use :cl)
   (:local-nicknames
    (:p :lparallel)
-   (:fp :cl-fp/api)
+   (:api :cl-fp/api)
    (:cons :cl-fp/cons))
   (:export
    :lazy-seq))
 
 (in-package cl-fp/lazy)
 
-(defclass lazy-seq (fp:collection)
+(defclass lazy-seq (api:collection)
   ((delay :initarg :delay :reader get-delay)))
 
 (defmacro lazy-seq (seq-form)
   `(make-instance 'lazy-seq :delay (p:delay ,seq-form)))
 
-(defmethod fp:conj ((lazy-seq lazy-seq) val)
+(defmethod api:conj ((lazy-seq lazy-seq) val)
   (cons:cons val lazy-seq))
 
-(defmethod fp:empty ((lazy-seq lazy-seq))
+(defmethod api:empty ((lazy-seq lazy-seq))
   (cons:list))
 
-(defmethod fp:first ((lazy-seq lazy-seq))
-  (fp:first (p:force (get-delay lazy-seq))))
+(defmethod api:first ((lazy-seq lazy-seq))
+  (api:first (p:force (get-delay lazy-seq))))
 
-(defmethod fp:rest ((lazy-seq lazy-seq))
-  (fp:rest (p:force (get-delay lazy-seq))))
+(defmethod api:rest ((lazy-seq lazy-seq))
+  (api:rest (p:force (get-delay lazy-seq))))
