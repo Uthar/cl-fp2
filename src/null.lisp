5681058bc79ef9f6ebb72feb650a4a395ab87bd7
Kasper Gałkowski <k@galkowski.xyz>
2023-06-26 19:53:40 +0200

    more null stuff

--- src/null.lisp
+++ src/null.lisp
@@ -1,9 +1,16 @@
 (defpackage cl-fp/null
   (:use :cl)
   (:local-nicknames
-   (:api :cl-fp/api)))
+   (:api :cl-fp/api)
+   (:cons :cl-fp/cons)))
 
 (in-package cl-fp/null)
 
 (defmethod api:first ((coll null))
+  nil)
+
+(defmethod api:rest ((coll null))
+  cons::+empty-list+)
+
+(defmethod api:next ((coll null))
   nil)
